c5172169f1470d97c284e17f9dc05f352cdf441d
Venkata Tirumalaraju <venkatasurya.db@gmail.com>
2018-07-28 20:29:55 -0400

    initial laravel application with a static page and about

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About</title>
+</head>
+<body>
+    <h1>About</h1>
+    <p>This is the application building in the <a href="https://selftaughtcoders.com/from-idea-to-launch/">From Idea to Launch </a>Course.</p>
+    <p>This web application will allow people to both submit, and answer, programming-related quetions for various different programming languages.</p>
+</body>
+</html>
